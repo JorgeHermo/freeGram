c1d61c8ef524b88280b78140e019285cd5bd9d45
Jorge <jhermo@SECUOYA.COM>
2023-03-15 12:37:30 +0100

    creating models for Posts & Profile + migrations Post & Profile + User model & table update + views modifications + Profile controller + Register controller update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Models\Profile;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -52,6 +53,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'username' => ['required', 'string', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -64,10 +66,17 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'username' => $data['username'],
             'password' => Hash::make($data['password']),
         ]);
+    
+        // Create a new profile for the user
+        $profile = new Profile();
+        $user->profile()->save($profile);
+    
+        return $user;
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,23 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3 p-5"><img src="https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3" style="height: 50%;" alt="freeGram Logo" class="rounded-circle"></div>
-        <div class="col-9">
-            <div><h1>_freeGram</h1></div>
-            <div>
-                <div><strong>150</strong> posts</div>
-                <div><strong>20k</strong> followers</div>
-                <div><strong>1k</strong> following</div>
+        <div class="col-3 p-1 d-flex justify-content-center align-items-center"><img src="https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3" alt="freeGram Logo" style="width: 200px; height: 200px;" class="rounded-circle img-fluid"></div>
+        <div class="col-9 pt-1">
+            <div class="ps-3"><h1>{{$user->username}}</h1></div>
+            <div class="d-flex">
+                <div class="ps-3"><strong>350</strong> posts</div>
+                <div class="ps-3"><strong>30k</strong> followers</div>
+                <div class="ps-3"><strong>1k</strong> following</div>
             </div>
+            <div class="pt-4 ps-3 font-weight-bold">{{$user->profile->title}}</div>
+            <div class="ps-3">{{$user->profile->description}}</div>
+            <div class="ps-3"><a href="#">{{$user->profile->url}}</a></div>
         </div>
+    </div>
+    <div class="row">
+        <div class="col-4 mt-5 p-4"><img src="https://static.freeimages.com/images/home/filetypes/photo.png" class="w-1 img-fluid"></div>
+        <div class="col-4 mt-5 p-4"><img src="https://static.freeimages.com/images/home/filetypes/photo.png" class="w-1 img-fluid"></div>
+        <div class="col-4 mt-5 p-4"><img src="https://static.freeimages.com/images/home/filetypes/photo.png" class="w-1 img-fluid"></div>
     </div>
 </div>
 @endsection
